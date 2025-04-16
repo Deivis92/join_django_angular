dad489e5c338a1a02ad2f21f8baff959d9f24a6b
Deividas Kondratjevas <deividaskondgmail.com>
2025-04-16 09:37:19 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SummaryComponent } from './main/summary/summary.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, SummaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
