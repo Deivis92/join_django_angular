98071db0bc8227bef715b88eaef5e540e10abff6
Deividas Kondratjevas <deividaskondgmail.com>
2025-04-18 11:45:12 +0300

    starting to remove old css

--- src/app/services/config-service.ts
+++ src/app/services/config-service.ts
@@ -3,30 +3,30 @@ import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
 export class ConfigService {
-    private apiUrl = 'http://127.0.0.1:8000/api/contacts/';
+  private apiUrl = 'http://127.0.0.1:8000/api/contacts/';
 
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-    getContacts(): Observable<any[]> {
-        return this.http.get<any[]>(this.apiUrl);
-      }
+  getContacts(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 
-    createContact(contact: any): Observable<any> {
-        return this.http.post<any>(this.apiUrl, contact);
-      }
+  createContact(contact: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, contact);
+  }
 
-      updateContact(contact: any): Observable<any> {
-        const url = `${this.apiUrl}${contact.id}/`;
-        return this.http.put<any>(url, contact);
-      }
+  updateContact(contact: any): Observable<any> {
+    const url = `${this.apiUrl}${contact.id}/`;
+    return this.http.put<any>(url, contact);
+  }
 
-      deleteContact(id: number): Observable<any> {
-        const url = `${this.apiUrl}${id}/`;
-        return this.http.delete<any>(url);
-      }
+  deleteContact(id: number): Observable<any> {
+    const url = `${this.apiUrl}${id}/`;
+    return this.http.delete<any>(url);
+  }
 }
